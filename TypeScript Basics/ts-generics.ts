f3d6016a99b383af6471c678095f05a99daeadb3
Anna Tasheva <anna.tasheva90@gmail.com>
2024-09-02 12:30:56 +0100

    Added more on Generics

--- TypeScript Basics/ts-generics.ts
+++ TypeScript Basics/ts-generics.ts
@@ -1,3 +1,5 @@
+// GENERICS in a Function
+
 function getFirstElement<ElementType>(arr: ElementType[]) {
   return arr[0];
 }
@@ -8,6 +10,8 @@ const firstNum = getFirstElement(numbers);
 const strings = ["one", "two", "three"];
 const firstString = getFirstElement(strings);
 
+// GENERICS in a Type
+
 type ApiResponse<Data> = {
   data: Data;
   isError: boolean;
@@ -17,6 +21,7 @@ type UserResponse = ApiResponse<{ name: string; age: number }>;
 type BlogResponse = ApiResponse<{ title: string }>;
 
 const response: UserResponse = {
+  // hover over 'UserResponse' and 'data'
   data: {
     name: "Kyle",
     age: 28,
@@ -25,8 +30,71 @@ const response: UserResponse = {
 };
 
 const responseBlog: BlogResponse = {
+  // hover over 'BlogResponse' and 'data'
   data: {
     title: "My Blog",
   },
   isError: false,
 };
+
+// GENERICS with a Default Value
+
+type ServerResponse<StatusData = { status: number }> = {
+  data: StatusData;
+  isError: boolean;
+};
+
+const responseFromServer1: ServerResponse = {
+  // hover over 'data'
+  data: {
+    status: 200,
+  },
+  isError: false,
+};
+
+// Overwriting the Default Value of the Generic
+
+const responseFromServer2: ServerResponse<{ status: string }> = {
+  // hover over 'data'
+  data: {
+    status: "200",
+  },
+  isError: false,
+};
+
+// Ensuring that <Data> generic is always an object and cannot be anything else
+
+type StrictApiResponse<Data extends object> = {
+  data: Data;
+  isError: false;
+};
+
+const strictResponse: StrictApiResponse<{ name: string }> = {
+  // If 'StrictApiResponse<string>' => we get and error: 'Type 'string' does not satisfy the constraint 'object'. '
+  data: {
+    name: "hello",
+  },
+  isError: false,
+};
+
+// Ensuring that <Data> generic is always an object with DEFAULT VALUE
+
+type strictResponseWithDefault<Data extends object = { status: number }> = {
+  data: Data;
+  isError: boolean;
+};
+
+const responseStrictDefault: strictResponseWithDefault = {
+  data: {
+    status: 200,
+  },
+  isError: false,
+};
+
+const responseOverwritingDefault: strictResponseWithDefault<{ name: string }> =
+  {
+    data: {
+      name: "hello",
+    },
+    isError: false,
+  };
